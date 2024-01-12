26767db4d2926c37d231936a097e8eab6bc92814
alex <al3xback@gmail.com>
2024-01-12 18:29:58 +0700

    added first commit

---
+++ test/main.js
@@ -0,0 +1,59 @@
+import { should } from 'chai';
+import jsdom from 'jsdom';
+import got from 'got';
+
+should();
+
+const { JSDOM } = jsdom;
+
+const url = 'https://al3xback.github.io/fmentor-stats-preview-mocha-chai-should/';
+
+const getData = () => {
+	return got(url)
+		.then((res) => {
+			const { document } = new JSDOM(res.body).window;
+			return document;
+		})
+		.catch((err) => {
+			throw new Error(err);
+		});
+};
+
+describe('DOM', () => {
+	beforeEach(async () => {
+		try {
+			const document = await getData();
+			global.document = document;
+		} catch (err) {
+			console.log(err);
+		}
+	});
+
+	it('should have a string type of heading one content element', () => {
+		const headingOneEl = document.querySelector('h1');
+		const headingOneContent = headingOneEl.textContent;
+
+		headingOneContent.should.be.a('string');
+	});
+
+	it("should have a mark element that contains 'insights' word", () => {
+		const cardTitleEl = document.querySelector('.card__title');
+		const cardMark = cardTitleEl.querySelector('mark').textContent;
+
+		cardMark.should.equal('insights');
+	});
+
+	it('should have two children inside of the article element', () => {
+		const articleEl = document.querySelector('article');
+		const articleChildrenEls = articleEl.children;
+
+		articleChildrenEls.should.have.lengthOf(2);
+	});
+
+	it('should have an empty alt attribute value of card image element', () => {
+		const cardImgEl = document.querySelector('.card__image img');
+		const cardImgAlt = cardImgEl.getAttribute('alt');
+
+		cardImgAlt.should.be.empty;
+	});
+});
